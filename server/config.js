474c51e6876b9d8bb8202ca3b076aa59f8244a82
Albin George <albingeorge@ulsecure-belknap-220235.wireless.louisville.edu>
2014-04-17 18:23:12 -0400

    configured it to have work on the deployed site at forthebabies.meteor.com

---
+++ server/config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+  service: "github"
+});
+
+ServiceConfiguration.configurations.insert({
+  service: "github",
+  clientId: "fc50e06bda9fac4f6e77",
+  secret: "2c5310d2dc3b16f15124659bbaf7c91238a6599a"
+});
